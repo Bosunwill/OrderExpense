4e5eafdc8697521588e331c9c46ed089e63927f6
Bosunwill <54271663+Bosunwill@users.noreply.github.com>
2021-05-05 08:17:50 -0500

    Initial Commit

---
+++ OrderExpense/OrderExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderExpenseApp.swift
+//  OrderExpense
+//
+//  Created by Gui Ndikum on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct OrderExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
